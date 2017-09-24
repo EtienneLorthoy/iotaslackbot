2286f0ecb84639f5553055b43acdbaf326df616a
Etienne Lorthoy <etienne.lorthoy@gsoft.com>
2017-09-24 09:56:56 -0400

    Add test project; Remove docker

---
+++ IOTA.Slackbot.Tests/IotaManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IOTA.Slackbot.Engine;
+using IOTA.Slackbot.Iota;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IOTA.Slackbot.Tests
+{
+    [TestClass]
+    public class IotaManagerTests
+    {
+        private IIotaManager _iotaManager;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            IOptions<IotaBotSettings> options = Options.Create(new IotaBotSettings
+            {
+                IotaNodePort = 14265,
+                IotaNodeUrl = "node.iotawallet.info",
+                IotaWalletSeed = "IOTASLACKBOTIOTASLACKBOTIOTASLACKBOTIOTASLACKBOTIOTASLACKBOTIOTASLACKBOTIOTASLA",
+                SlackBotToken = string.Empty
+            });
+
+            this._iotaManager = new IotaManager(options);
+        }
+
+        [TestMethod]
+        public void CreateAddress_returnAddress()
+        {
+            var address = _iotaManager.CreateAddress();
+
+            Assert.IsNotNull(address);
+        }
+    }
+}
--- IOTA.Slackbot/Controllers/PingController.cs
+++ IOTA.Slackbot/Controllers/PingController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using FluentScheduler;
 
 namespace IOTA.Slackbot.Controllers
 {
@@ -10,6 +13,18 @@ namespace IOTA.Slackbot.Controllers
         public string Get()
         {
             return "Pong...";
+        }
+
+        [HttpGet("JobStatus")]
+        public IEnumerable<string> JobStatus()
+        {
+            var result = new List<string> {"* All Schedules *"};
+            result.AddRange(JobManager.AllSchedules.Select(x => $"{x.Name}:{x.NextRun}"));
+
+            result.Add("* RunningSchedules *");
+            result.AddRange(JobManager.RunningSchedules.Select(x => $"{x.Name}:{x.NextRun}"));
+
+            return result;
         }
     }
 }
