b480bd20a7b453202804d10a3574eae5bdca7569
Olivier Foucherot <ofoucherot@gmail.com>
2017-10-15 16:38:36 -0400

    add slack basic

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const request = require('request');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
 const app = express()
+const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello tipwallet!')
@@ -10,7 +18,26 @@ app.post('/api/tipwallet/info', function (req, res) {
 })
 
 app.post('/api/tipwallet/deposite', function (req, res) {
-    res.send('tipwallet deposite!')
+
+    console.log(req.body);
+
+    if (req.body.token === process.env.SLACK_VERIFICATION_TOKEN)  {
+
+        request.post(
+            req.bodyresponse_url,
+            { json: { text: 'test deposite' } },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                }
+            }
+        );
+
+        res.send('');
+        return;
+    }
+
+    res.status(500).send('invalid token')
 })
 
 app.post('/api/tipwallet/withdraw', function (req, res) {
@@ -21,7 +48,6 @@ app.post('/api/tipwallet/sendtip', function (req, res) {
     res.send('tipwallet sendtip!')
 })
 
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, function () {
+    console.log(`Server is listening on port ${PORT}`)
 })
