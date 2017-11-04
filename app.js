9787dcd65ed046a3bc34ca1207c520922e1f6b1a
Olivier Foucherot <ofoucherot@gmail.com>
2017-11-04 17:22:20 -0400

    refac

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000;
+var db;
+var agenda;
 
 var MongoClient = require('mongodb').MongoClient;
 var mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
@@ -16,18 +18,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', iotaManager.getNodeInfo)
 
-MongoClient.connect(mongoConnectionString, function (err, db) {
-    console.log("Connected successfully to server");
+MongoClient.connect(mongoConnectionString, function (err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
+    db = database;
 
-    db.close();
-});
+    console.log("Database connection ready");
 
-var agenda = new Agenda({ db: { address: mongoConnectionString, collection: 'jobs' } });
+    app.listen(PORT, function () {
+        console.log(`Server is running on port ${PORT}`)
+    });
 
-agenda.on('ready', function () {
-    agenda.every('3 minutes', 'test job');
-    agenda.start();
+    agenda = new Agenda({ db: { address: mongoConnectionString, collection: 'jobs' } });
+
+    agenda.on('ready', function () {
+        agenda.every('3 minutes', 'test job');
+        agenda.start();
+    });
 });
 
 app.post('/api/tipwallet/info', function (req, res) {
@@ -65,6 +75,3 @@ app.post('/api/tipwallet/sendtip', function (req, res) {
     res.send('tipwallet sendtip!')
 })
 
-app.listen(PORT, function () {
-    console.log(`Server is listening on port ${PORT}`)
-})
